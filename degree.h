0b6310ca85c0cf31f6e59fc411b8f828bc486234
AnaFrei-007 <anafrei2024@gmail.com>
2024-10-18 11:15:57 +0100

    Delete degree.h

--- degree.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-using namespace std;
-
-
-enum DegreeProgram {SECURITY, NETWORK, SOFTWARE};
-
-static const string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" };
